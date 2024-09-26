c9db162b29f68567950a73991437e6358b9e92eb
Aarnow <arnaudfgr.dev@gmail.com>
2024-09-26 04:33:03 +0200

    Initial Project Files

---
+++ JobImpound.cs
@@ -0,0 +1,27 @@
+﻿using Life;
+using Life.Network;
+using Life.UI;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using System.Collections.Generic;
+using _menu = AAMenu.Menu;
+using mk = ModKit.Helper.TextFormattingHelper;
+
+namespace JobImpound
+{
+    public class JobImpound : ModKit.ModKit
+    {
+
+        public JobImpound(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+        
+            ModKit.Internal.Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
